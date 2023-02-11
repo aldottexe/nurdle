76fab666a602f66df74d3390356fde0ec25ad986
aldottexe <aldottexe@gmail.com>
2023-02-11 15:22:10 -0500

    optimized parseColor

--- src/routes/Guess.svelte
+++ src/routes/Guess.svelte
@@ -2,14 +2,16 @@
    export let guess;
    export let key;
 
-   function parseColor(char, i){
-      if(char === '')
+   function parseColor(i){
+      let guessChar = guess.charAt(i);
+
+      if(guessChar === '')
          return '';
 
-      if (key.charAt(i) === char)
+      if (guessChar === key.charAt(i))
          return 'green';
 
-      if(key.indexOf(char) != -1)
+      if(key.indexOf(guessChar) != -1)
          return 'yellow';
 
       return '';
@@ -19,18 +21,18 @@
 
 <div>
    <h3>(</h3>
-   <h3 class="letter {parseColor(guess.charAt(0), 0)}">{guess.charAt(0)}</h3>
-   <h3 class="letter {parseColor(guess.charAt(1), 1)}">{guess.charAt(1)}</h3>
-   <h3 class="letter {parseColor(guess.charAt(2), 2)}">{guess.charAt(2)}</h3>
+   <h3 class="letter {parseColor(0)}">{guess.charAt(0)}</h3>
+   <h3 class="letter {parseColor(1)}">{guess.charAt(1)}</h3>
+   <h3 class="letter {parseColor(2)}">{guess.charAt(2)}</h3>
    <h3>)</h3>
-   <h3 class="letter {parseColor(guess.charAt(3), 3)}">{guess.charAt(3)}</h3>
-   <h3 class="letter {parseColor(guess.charAt(4), 4)}">{guess.charAt(4)}</h3>
-   <h3 class="letter {parseColor(guess.charAt(5), 5)}">{guess.charAt(5)}</h3>
+   <h3 class="letter {parseColor(3)}">{guess.charAt(3)}</h3>
+   <h3 class="letter {parseColor(4)}">{guess.charAt(4)}</h3>
+   <h3 class="letter {parseColor(5)}">{guess.charAt(5)}</h3>
    <h3>-</h3>
-   <h3 class="letter {parseColor(guess.charAt(6), 6)}">{guess.charAt(6)}</h3>
-   <h3 class="letter {parseColor(guess.charAt(7), 7)}">{guess.charAt(7)}</h3>
-   <h3 class="letter {parseColor(guess.charAt(8), 8)}">{guess.charAt(8)}</h3>
-   <h3 class="letter {parseColor(guess.charAt(9), 9)}">{guess.charAt(9)}</h3>
+   <h3 class="letter {parseColor(6)}">{guess.charAt(6)}</h3>
+   <h3 class="letter {parseColor(7)}">{guess.charAt(7)}</h3>
+   <h3 class="letter {parseColor(8)}">{guess.charAt(8)}</h3>
+   <h3 class="letter {parseColor(9)}">{guess.charAt(9)}</h3>
 </div>
 
 <style>
