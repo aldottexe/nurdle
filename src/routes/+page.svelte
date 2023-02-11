cb72130d87b7a3339307afdad756b7330a1cfb56
aldottexe <aldottexe@gmail.com>
2023-02-11 14:55:30 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,81 @@
+<script>
+   import Guess from './Guess.svelte';
+   let guesses = [];
+   let key = '3023672036'
+   let won = false;
+
+   let val = '';
+   $: val = validateEntry(val);
+
+   function validateEntry(val){
+      if(val.length > 10 
+         || val.charAt(val.length-1) < '0' 
+         || val.charAt(val.length-1) > '9')
+         return val.slice(0, val.length-1);
+      return val;
+   }
+
+   function addNewGuess(e){
+      e.preventDefault();
+      if(val === key)
+         won = true;
+
+      guesses = [...guesses, val];
+      val = '';
+   }
+</script>
+
+<section class="win {won ? 'won':'hide'}">
+   <h1>ggez</h1>
+</section>
+
+<h1>Nurdle</h1>
+<p>For those who play hard to get.</p>
+
+{#each guesses as guess}
+<Guess key={key} guess={guess}></Guess>
+{/each}
+
+<form action="" on:submit={addNewGuess}>
+   <input type="text" bind:value={val} />
+   <Guess key="x" guess={val}></Guess>
+   <input type="submit" hidden >
+</form>
+
+<style>
+   *{
+      color: white;
+      font-family: "comic sans MS";
+      text-align: center;
+   }
+   input{
+      position: absolute;
+      left: 0;
+      opacity: 0%;
+      width: 100vw;
+      height: 100px;
+   }
+   form{
+      padding-top: 7vh
+   }
+   .win{
+      height: 100vh;
+      width: 100vw;
+      position:fixed;
+      top:0;
+      left: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: rgba(94, 255, 201, 0.632);
+      font-size: 3em;
+      transition: opacity 1s;
+   }
+   .hide{
+      opacity: 0;
+      visibility: hidden;
+   }
+   .won{
+      opacity: 100%;
+   }
+</style>
---
+++ src/routes/Guess.svelte
@@ -0,0 +1,63 @@
+<script>
+   export let guess;
+   export let key;
+
+   function parseColor(char, i){
+      if(char === '')
+         return '';
+
+      if (key.charAt(i) === char)
+         return 'green';
+
+      if(key.indexOf(char) != -1)
+         return 'yellow';
+
+      return '';
+   }
+
+</script>
+
+<div>
+   <h3>(</h3>
+   <h3 class="letter {parseColor(guess.charAt(0), 0)}">{guess.charAt(0)}</h3>
+   <h3 class="letter {parseColor(guess.charAt(1), 1)}">{guess.charAt(1)}</h3>
+   <h3 class="letter {parseColor(guess.charAt(2), 2)}">{guess.charAt(2)}</h3>
+   <h3>)</h3>
+   <h3 class="letter {parseColor(guess.charAt(3), 3)}">{guess.charAt(3)}</h3>
+   <h3 class="letter {parseColor(guess.charAt(4), 4)}">{guess.charAt(4)}</h3>
+   <h3 class="letter {parseColor(guess.charAt(5), 5)}">{guess.charAt(5)}</h3>
+   <h3>-</h3>
+   <h3 class="letter {parseColor(guess.charAt(6), 6)}">{guess.charAt(6)}</h3>
+   <h3 class="letter {parseColor(guess.charAt(7), 7)}">{guess.charAt(7)}</h3>
+   <h3 class="letter {parseColor(guess.charAt(8), 8)}">{guess.charAt(8)}</h3>
+   <h3 class="letter {parseColor(guess.charAt(9), 9)}">{guess.charAt(9)}</h3>
+</div>
+
+<style>
+   *{
+      color: white;
+      font-family: "comic sans MS";
+   }
+   div{
+      display: flex;
+      width: fit-content;
+      margin: 20px auto;
+   }
+   div *{
+      width: 2em;
+      height: 2em;
+      text-align: center;
+      margin: 3px;
+      padding-top: 3px;
+      box-sizing: border-box;
+   }
+   .letter{
+      border: 2px solid rgba(255, 255, 255, 0.3);
+   }
+   .yellow{
+      background-color: darkgoldenrod;
+   }
+   .green{
+      background-color: darkcyan;
+   }
+</style>
