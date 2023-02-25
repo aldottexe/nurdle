0484f8800c57312f6138e20fd6f07e6aa2562413
aldottexe <aldottexe@gmail.com>
2023-02-25 11:33:25 -0500

    readded build files and added ghpages module

---
+++ build/_app/immutable/chunks/1-20be7b69.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-9f063a0a.js";export{t as component};
---
+++ build/_app/immutable/chunks/2-24a06ecc.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-e65dc72c.js";export{t as component};
